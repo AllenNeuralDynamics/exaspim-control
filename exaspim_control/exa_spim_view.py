543f4df2da8a48f493b9c17cde93424a297adde1
Micah Woodard <micah.woodard@alleninstitute.org>
2024-08-08 08:12:35 -0700

    feat: move saving config to view repo

--- exaspim_control/exa_spim_view.py
+++ exaspim_control/exa_spim_view.py
@@ -1,4 +1,4 @@
-from qtpy.QtWidgets import QApplication, QMessageBox, QPushButton, QFileDialog
+from qtpy.QtWidgets import QApplication
 from view.instrument_view import InstrumentView
 from view.acquisition_view import AcquisitionView
 from pathlib import Path
@@ -47,47 +47,8 @@ class ExASPIMInstrumentView(InstrumentView):
                     layer.events.contrast_limits.connect(lambda event: self.contrastChanged.emit(np.rot90(layer.data[-3], k=3),
                                                                                                  layer.contrast_limits))
 
-    def update_config_on_quit(self):
-        """Add functionality to close function to save device properties to instrument config"""
-
-        return_value = self.update_config_query()
-        if return_value == QMessageBox.Ok:
-            self.instrument.update_current_state_config()
-            self.instrument.save_config(self.config_save_to)
-
-    def update_config_query(self):
-        """Pop up message asking if configuration would like to be saved"""
-        msgBox = QMessageBox()
-        msgBox.setIcon(QMessageBox.Question)
-        msgBox.setText(f"Do you want to update the instrument configuration file at {self.config_save_to} "
-                       f"to current instrument state?")
-        msgBox.setWindowTitle("Updating Configuration")
-        msgBox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
-        save_elsewhere = QPushButton('Change Directory')
-        msgBox.addButton(save_elsewhere, QMessageBox.DestructiveRole)
-
-        save_elsewhere.pressed.connect(lambda: self.select_directory(True, msgBox))
-
-        return msgBox.exec()
-
-    def select_directory(self, pressed, msgBox):
-        """Select directory"""
-
-        fname = QFileDialog()
-        folder = fname.getSaveFileName(directory=str(self.instrument.config_path))
-        if folder[0] != '': # user pressed cancel
-            msgBox.setText(f"Do you want to update the instrument configuration file at {folder[0]} "
-                           f"to current instrument state?")
-            self.config_save_to = Path(folder[0])
-
 class ExASPIMAcquisitionView(AcquisitionView):
     """View for ExASPIM Acquisition"""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        QApplication.instance().aboutToQuit.connect(self.update_config_on_quit)
-        self.config_save_to = self.acquisition.config_path
 
     def update_acquisition_layer(self, args):
         """Update viewer with latest frame taken during acquisition
@@ -98,35 +59,3 @@ class ExASPIMAcquisitionView(AcquisitionView):
         if image is not None:
             downsampled = skimage.measure.block_reduce(image, (4,4), np.mean)
             super().update_acquisition_layer((downsampled, camera_name))
-    def update_config_on_quit(self):
-        """Add functionality to close function to save device properties to instrument config"""
-
-        return_value = self.update_config_query()
-        if return_value == QMessageBox.Ok:
-            self.acquisition.update_current_state_config()
-            self.acquisition.save_config(self.config_save_to)
-
-    def update_config_query(self):
-        """Pop up message asking if configuration would like to be saved"""
-        msgBox = QMessageBox()
-        msgBox.setIcon(QMessageBox.Question)
-        msgBox.setText(f"Do you want to update the acquisition configuration file at {self.config_save_to} "
-                       f"to current acquisition state?")
-        msgBox.setWindowTitle("Updating Configuration")
-        msgBox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
-        save_elsewhere = QPushButton('Change Directory')
-        msgBox.addButton(save_elsewhere, QMessageBox.DestructiveRole)
-
-        save_elsewhere.pressed.connect(lambda: self.select_directory(True, msgBox))
-
-        return msgBox.exec()
-
-    def select_directory(self, pressed, msgBox):
-        """Select directory"""
-
-        fname = QFileDialog()
-        folder = fname.getSaveFileName(directory=str(self.acquisition.config_path))
-        if folder[0] != '': # user pressed cancel
-            msgBox.setText(f"Do you want to update the instrument configuration file at {folder[0]} "
-                           f"to current instrument state?")
-            self.config_save_to = Path(folder[0])
