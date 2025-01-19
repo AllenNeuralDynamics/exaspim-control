fb06394ef48af7e2e36a236bcf27d3a67c1596a0
adamkglaser <adamkglaser@gmail.com>
2025-01-19 10:31:45 -0500

    adding instrument view properties to gui_config yaml file

--- src/exaspim_control/exa_spim_view.py
+++ src/exaspim_control/exa_spim_view.py
@@ -5,10 +5,24 @@ from pathlib import Path
 import numpy as np
 from napari.utils.theme import get_theme
 from qtpy.QtCore import Qt, Signal
-from qtpy.QtWidgets import QFrame, QLabel, QPushButton, QScrollArea, QVBoxLayout
+from qtpy.QtWidgets import (
+    QFrame,
+    QGridLayout,
+    QHBoxLayout,
+    QLabel,
+    QPushButton,
+    QScrollArea,
+    QSizePolicy,
+    QSplitter,
+    QVBoxLayout,
+    QWidget,
+)
 
 from view.acquisition_view import AcquisitionView
 from view.instrument_view import InstrumentView
+from view.widgets.acquisition_widgets.channel_plan_widget import ChannelPlanWidget
+from view.widgets.acquisition_widgets.volume_model import VolumeModel
+from view.widgets.acquisition_widgets.volume_plan_widget import VolumePlanWidget
 from view.widgets.base_device_widget import create_widget, disable_button
 from voxel.processes.downsample.gpu.gputools.rank_downsample_2d import GPUToolsRankDownSample2D
 
@@ -33,11 +47,21 @@ class ExASPIMInstrumentView(InstrumentView):
         self.setup_flip_mount_widgets()
 
         # viewer constants for ExA-SPIM
-        self.intensity_min = 30
-        self.intensity_max = 400
-        self.camera_rotation = -90
-        self.resolution_levels = 6
-        self.alignment_roi_size = 512
+        self.intensity_min = self.config["instrument_view"]["properties"]["intensity_min"]
+        if self.intensity_min < 0 or self.intensity_min > 65535:
+            raise ValueError("intensity min must be between 0 and 65535")
+        self.intensity_max = self.config["instrument_view"]["properties"]["intensity_max"]
+        if self.intensity_max < self.intensity_min or self.intensity_max > 65535:
+            raise ValueError("intensity max must be between intensity min and 65535")
+        self.camera_rotation = self.config["instrument_view"]["properties"]["camera_rotation_deg"]
+        if self.camera_rotation not in [0, 90, 180, 270, 360, -90, -180, -270]:
+            raise ValueError("camera rotation must be 0, 90, 180, 270, -90, -180, -270")
+        self.resolution_levels = self.config["instrument_view"]["properties"]["resolution_levels"]
+        if self.resolution_levels < 1 or self.resolution_levels > 10:
+            raise ValueError("resolution levels must be between 1 and 10")
+        self.alignment_roi_size = self.config["instrument_view"]["properties"]["alignment_roi_size"]
+        if self.alignment_roi_size < 2 or self.alignment_roi_size > 1024:
+            raise ValueError("alignment roi size must be between 2 and 1024")
         self.viewer.scale_bar.visible = True
         self.viewer.scale_bar.unit = "um"
         self.viewer.scale_bar.position = "bottom_left"
@@ -146,18 +170,18 @@ class ExASPIMInstrumentView(InstrumentView):
             _ = self.viewer.layers
             # add crosshairs to image
             if self.crosshairs_button.isChecked():
-                image[image.shape[0] // 2 - 1: image.shape[0] // 2 + 1, :] = 1 << 16 - 1
-                image[:, image.shape[1] // 2 - 1: image.shape[1] // 2 + 1] = 1 << 16 - 1
+                image[image.shape[0] // 2 - 1 : image.shape[0] // 2 + 1, :] = 1 << 16 - 1
+                image[:, image.shape[1] // 2 - 1 : image.shape[1] // 2 + 1] = 1 << 16 - 1
             multiscale = [image]
             downsampler = GPUToolsRankDownSample2D(binning=2, rank=-2, data_type="uint16")
             for binning in range(1, self.resolution_levels):
                 downsampled_frame = downsampler.run(multiscale[-1])
                 # add crosshairs to image
                 if self.crosshairs_button.isChecked():
-                    downsampled_frame[downsampled_frame.shape[0] // 2 - 1: downsampled_frame.shape[0] // 2 + 1, :] = (
+                    downsampled_frame[downsampled_frame.shape[0] // 2 - 1 : downsampled_frame.shape[0] // 2 + 1, :] = (
                         1 << 16 - 1
                     )
-                    downsampled_frame[:, downsampled_frame.shape[1] // 2 - 1: downsampled_frame.shape[1] // 2 + 1] = (
+                    downsampled_frame[:, downsampled_frame.shape[1] // 2 - 1 : downsampled_frame.shape[1] // 2 + 1] = (
                         1 << 16 - 1
                     )
                 multiscale.append(downsampled_frame)
@@ -213,49 +237,49 @@ class ExASPIMInstrumentView(InstrumentView):
             combined_roi[0:alignment_roi, 0:alignment_roi] = top_left
             # top right corner
             top_right = image[0:alignment_roi, -alignment_roi:]
-            combined_roi[0:alignment_roi, alignment_roi * 2: alignment_roi * 3] = top_right
+            combined_roi[0:alignment_roi, alignment_roi * 2 : alignment_roi * 3] = top_right
             # bottom left corner
             bottom_left = image[-alignment_roi:, 0:alignment_roi]
-            combined_roi[alignment_roi * 2: alignment_roi * 3, 0:alignment_roi] = bottom_left
+            combined_roi[alignment_roi * 2 : alignment_roi * 3, 0:alignment_roi] = bottom_left
             # bottom right corner
             bottom_right = image[-alignment_roi:, -alignment_roi:]
-            combined_roi[alignment_roi * 2: alignment_roi * 3, alignment_roi * 2: alignment_roi * 3] = bottom_right
+            combined_roi[alignment_roi * 2 : alignment_roi * 3, alignment_roi * 2 : alignment_roi * 3] = bottom_right
             # center left
             center_left = image[
-                round((image.shape[0] / 2) - alignment_roi / 2): round((image.shape[0] / 2) + alignment_roi / 2),
+                round((image.shape[0] / 2) - alignment_roi / 2) : round((image.shape[0] / 2) + alignment_roi / 2),
                 0:alignment_roi,
             ]
-            combined_roi[alignment_roi: alignment_roi * 2, 0:alignment_roi] = center_left
+            combined_roi[alignment_roi : alignment_roi * 2, 0:alignment_roi] = center_left
             # center right
             center_right = image[
-                round((image.shape[0] / 2) - alignment_roi / 2): round((image.shape[0] / 2) + alignment_roi / 2),
+                round((image.shape[0] / 2) - alignment_roi / 2) : round((image.shape[0] / 2) + alignment_roi / 2),
                 -alignment_roi:,
             ]
-            combined_roi[alignment_roi: alignment_roi * 2, alignment_roi * 2: alignment_roi * 3] = center_right
+            combined_roi[alignment_roi : alignment_roi * 2, alignment_roi * 2 : alignment_roi * 3] = center_right
             # center top
             center_top = image[
                 0:alignment_roi,
-                round((image.shape[1] / 2) - alignment_roi / 2): round((image.shape[1] / 2) + alignment_roi / 2),
+                round((image.shape[1] / 2) - alignment_roi / 2) : round((image.shape[1] / 2) + alignment_roi / 2),
             ]
-            combined_roi[0:alignment_roi, alignment_roi: alignment_roi * 2] = center_top
+            combined_roi[0:alignment_roi, alignment_roi : alignment_roi * 2] = center_top
             # center bottom
             center_bottom = image[
                 -alignment_roi:,
-                round((image.shape[1] / 2) - alignment_roi / 2): round((image.shape[1] / 2) + alignment_roi / 2),
+                round((image.shape[1] / 2) - alignment_roi / 2) : round((image.shape[1] / 2) + alignment_roi / 2),
             ]
-            combined_roi[alignment_roi * 2: alignment_roi * 3, alignment_roi: alignment_roi * 2] = center_bottom
+            combined_roi[alignment_roi * 2 : alignment_roi * 3, alignment_roi : alignment_roi * 2] = center_bottom
             # center roi
             center = image[
-                round((image.shape[0] / 2) - alignment_roi / 2): round((image.shape[0] / 2) + alignment_roi / 2),
-                round((image.shape[1] / 2) - alignment_roi / 2): round((image.shape[1] / 2) + alignment_roi / 2),
+                round((image.shape[0] / 2) - alignment_roi / 2) : round((image.shape[0] / 2) + alignment_roi / 2),
+                round((image.shape[1] / 2) - alignment_roi / 2) : round((image.shape[1] / 2) + alignment_roi / 2),
             ]
-            combined_roi[alignment_roi: alignment_roi * 2, alignment_roi: alignment_roi * 2] = center
+            combined_roi[alignment_roi : alignment_roi * 2, alignment_roi : alignment_roi * 2] = center
 
             # add crosshairs to image
-            combined_roi[alignment_roi - 2: alignment_roi + 2, :] = 1 << 16 - 1
-            combined_roi[alignment_roi * 2 - 2: alignment_roi * 2 + 2, :] = 1 << 16 - 1
-            combined_roi[:, alignment_roi - 2: alignment_roi + 2] = 1 << 16 - 1
-            combined_roi[:, alignment_roi * 2 - 2: alignment_roi * 2 + 2] = 1 << 16 - 1
+            combined_roi[alignment_roi - 2 : alignment_roi + 2, :] = 1 << 16 - 1
+            combined_roi[alignment_roi * 2 - 2 : alignment_roi * 2 + 2, :] = 1 << 16 - 1
+            combined_roi[:, alignment_roi - 2 : alignment_roi + 2] = 1 << 16 - 1
+            combined_roi[:, alignment_roi * 2 - 2 : alignment_roi * 2 + 2] = 1 << 16 - 1
 
             layer_name = f"{camera_name} {self.livestream_channel} Alignment"
             if layer_name in self.viewer.layers:
@@ -322,6 +346,109 @@ class ExASPIMAcquisitionView(AcquisitionView):
         super().__init__(acquisition=acquisition, instrument_view=instrument_view)
         # acquisition constants for ExA-SPIM
         self.acquisition_binning = 4
+
+    def create_acquisition_widget(self) -> QSplitter:
+        """
+        Create the acquisition widget.
+
+        :raises KeyError: If the coordinate plane does not match instrument axes in tiling_stages
+        :return: Acquisition widget
+        :rtype: QSplitter
+        """
+        # find limits of all axes
+        lim_dict = {}
+        # add tiling stages
+        for name, stage in self.instrument.tiling_stages.items():
+            lim_dict.update({f"{stage.instrument_axis}": stage.limits_mm})
+        # last axis should be scanning axis
+        ((scan_name, scan_stage),) = self.instrument.scanning_stages.items()
+        lim_dict.update({f"{scan_stage.instrument_axis}": scan_stage.limits_mm})
+        try:
+            limits = [lim_dict[x.strip("-")] for x in self.coordinate_plane]
+        except KeyError:
+            raise KeyError("Coordinate plane must match instrument axes in tiling_stages")
+
+        # TODO fix this, messy way to figure out FOV dimensions from camera properties
+        first_camera_key = list(self.instrument.cameras.keys())[0]
+        camera = self.instrument.cameras[first_camera_key]
+        fov_height_mm = camera.fov_height_mm
+        fov_width_mm = camera.fov_width_mm
+        camera_rotation = self.config["instrument_view"]["properties"]["camera_rotation_deg"]
+        if camera_rotation in [-270, -90, 90, 270]:
+            fov_dimensions = [fov_height_mm, fov_width_mm, 0]
+        else:
+            fov_dimensions = [fov_width_mm, fov_height_mm, 0]
+
+        acquisition_widget = QSplitter(Qt.Vertical)
+        acquisition_widget.setChildrenCollapsible(False)
+
+        # create volume plan
+        self.volume_plan = VolumePlanWidget(
+            limits=limits, fov_dimensions=fov_dimensions, coordinate_plane=self.coordinate_plane, unit=self.unit
+        )
+        self.volume_plan.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Minimum)
+
+        # create volume model
+        self.volume_model = VolumeModel(
+            limits=limits,
+            fov_dimensions=fov_dimensions,
+            coordinate_plane=self.coordinate_plane,
+            unit=self.unit,
+            **self.config["acquisition_view"]["acquisition_widgets"].get("volume_model", {}).get("init", {}),
+        )
+        # combine floating volume_model widget with glwindow
+        combined_layout = QGridLayout()
+        combined_layout.addWidget(self.volume_model, 0, 0, 3, 1)
+        combined_layout.addWidget(self.volume_model.widgets, 3, 0, 1, 1)
+        combined = QWidget()
+        combined.setLayout(combined_layout)
+        acquisition_widget.addWidget(create_widget("H", self.volume_plan, combined))
+
+        # create channel plan
+        self.channel_plan = ChannelPlanWidget(
+            instrument_view=self.instrument_view,
+            channels=self.instrument.config["instrument"]["channels"],
+            unit=self.unit,
+            **self.config["acquisition_view"]["acquisition_widgets"].get("channel_plan", {}).get("init", {}),
+        )
+        # place volume_plan.tile_table and channel plan table side by side
+        table_splitter = QSplitter(Qt.Horizontal)
+        table_splitter.setChildrenCollapsible(False)
+        table_splitter.setHandleWidth(20)
+
+        widget = QWidget()  # dummy widget to move tile_table down in layout
+        widget.setMinimumHeight(25)
+        table_splitter.addWidget(create_widget("V", widget, self.volume_plan.tile_table))
+        table_splitter.addWidget(self.channel_plan)
+
+        # format splitter handle. Must do after all widgets are added
+        handle = table_splitter.handle(1)
+        handle_layout = QHBoxLayout(handle)
+        line = QFrame(handle)
+        line.setStyleSheet("QFrame {border: 1px dotted grey;}")
+        line.setFixedHeight(50)
+        line.setFrameShape(QFrame.VLine)
+        handle_layout.addWidget(line)
+
+        # add tables to layout
+        acquisition_widget.addWidget(table_splitter)
+
+        # connect signals
+        self.instrument_view.snapshotTaken.connect(self.volume_model.add_fov_image)  # connect snapshot signal
+        self.instrument_view.contrastChanged.connect(
+            self.volume_model.adjust_glimage_contrast
+        )  # connect snapshot adjusted
+        self.volume_model.fovHalt.connect(self.stop_stage)  # stop stage if halt button is pressed
+        self.volume_model.fovMove.connect(self.move_stage)  # move stage to clicked coords
+        self.volume_plan.valueChanged.connect(self.volume_plan_changed)
+        self.channel_plan.channelAdded.connect(self.channel_plan_changed)
+        self.channel_plan.channelChanged.connect(self.update_tiles)
+
+        # TODO: This feels like a clunky connection. Works for now but could probably be improved
+        self.volume_plan.header.startChanged.connect(lambda i: self.create_tile_list())
+        self.volume_plan.header.stopChanged.connect(lambda i: self.create_tile_list())
+
+        return acquisition_widget
 
     def update_acquisition_layer(self, image: np.ndarray, camera_name: str) -> None:
         """
